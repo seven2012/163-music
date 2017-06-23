963ea07c738b61c2aefb6b6e9ae021f5eef70f5f
july77 <ljlpsy11@gmail.com>
2017-06-23 16:09:52 +0800

    修改

--- song.js
+++ song.js
@@ -1,4 +1,29 @@
 $(function () {
+    $.get('/lyric.json').then(function (object) {
+        console.log(object)
+        let lyric = object.lyric
+        console.log(lyric)
+        let array = lyric.split('\n')
+        let regex = /^\[(.+)\](.*)$/
+        array = array.map(function (string) {
+            let matches = regex.exec(string)
+            if (matches) {
+                console.log(matches[1])
+                console.log(matches[2])
+                return { time: matches[1], words: matches[2] }
+            }
+
+        })
+
+        let $lyric = $('.lyric')
+        array.map(function (object) {
+            if (!object) { return }
+            let $p = $('<p/>')
+            $p.attr('data-time', object.time).text(object.words)
+            $p.appendTo($lyric.children('.lyric-scroll'))
+        })
+    })
+
 
     let audio = document.createElement('audio')
     audio.src = 'http://oplbcrrjd.bkt.clouddn.com/a1c6%252Fde3a%252F6efd%252F1b7e8ba1ffce645218a1748cd8f644ff.mp3'
@@ -18,30 +43,6 @@ $(function () {
     })
 
 
-    $.get('/lyric.json').then(function (object) {
-        console.log(object)
-        let lyric = object.lyric
-        console.log(lyric)
-        let array = lyric.split('\n')
-        let regex = /^\[(.+)\](.*)$/
-        array = array.map(function (string) {
-            let matches = regex.exec(string)
-            if (matches) {
-                console.log(matches[1])
-                console.log(matches[2])
-                return { time: matches[1], words: matches[2] }
-            }
-        
-        })
 
-    let $lyric = $('.lyric')
-    array.map(function (object) {
-        if (!object) { return }
-        let $p = $('<p/>')
-        $p.attr('data-time', object.time).text(object.words)
-        $p.appendTo($lyric.children('.lyric-scroll'))
-    })
-})
-    
-   
+
 })
